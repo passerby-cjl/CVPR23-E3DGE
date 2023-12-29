fa24c517f459831fc156048c22cb42ac405aed78
passerby-cjl <87693336+passerby-cjl@users.noreply.github.com>
2023-12-29 16:46:20 +0800

    Update warp_and_crop.py

--- warp_and_crop.py
+++ warp_and_crop.py
@@ -8,11 +8,10 @@ from facexlib.detection import init_detection_model
 
 def main(args):
     det_net = init_detection_model('retinaface_resnet50')
-    half = False
     det_net.to(args.device).eval()
     img = cv2.imread(args.img_path)
     with torch.no_grad():
-        bboxes, warped_face_list = det_net.align_multi(img, 0.97, half=half)
+        bboxes, warped_face_list = det_net.align_multi(img, 0.97)
     print(bboxes.shape,bboxes)
     print(len(warped_face_list))
     cv2.imwrite(args.save_path,warped_face_list[0])
